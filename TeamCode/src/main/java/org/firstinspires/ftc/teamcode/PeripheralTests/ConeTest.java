94d634e0bb5978d9aa71a1c3416223462df14247
Rohan Dugad <rohand2468@gmail.com>
2023-01-05 23:30:09 -0500

    Plain test for cone color code from Robot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PeripheralTests/ConeTest.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.PeripheralTests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Control.Robot;
+import org.firstinspires.ftc.teamcode.Control._Autonomous;
+
+@Autonomous(name="ConeTest", group="PeripheralTest")
+public class ConeTest extends _Autonomous {
+
+    private double _hue;
+    private String _color;
+
+    @Override
+    public void init() {
+        Robot.setup(hardwareMap, telemetry, Robot.SetupType.OpenCV);
+    }
+
+    @Override
+    public void init_loop() {
+        telemetry.addLine(Robot.getWebcam().getName());
+        telemetry.addLine("" + Robot.getPipeline().getConeHue());
+
+        _hue = Robot.getPipeline().getConeHue();
+    }
+
+    @Override
+    public void start() {
+        Robot.getWebcam().stopStreaming();
+        Robot.getWebcam().closeCameraDevice();
+
+        if (_hue <= 25) {
+            _color = "Red";
+        }
+        else if (_hue <= 75) {
+            _color = "Green";
+        }
+        else {
+            _color = "Blue";
+        }
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addLine(_color);
+    }
+}
