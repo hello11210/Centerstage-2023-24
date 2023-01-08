bc43bece084aff243c94b9ff3f45d781d9d9869c
Rohan Dugad <rohand2468@gmail.com>
2023-01-07 19:31:45 -0500

    Changed motor type from 435 to 312

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverTests/DrivetrainTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverTests/DrivetrainTest.java
@@ -24,13 +24,13 @@ public class DrivetrainTest extends _Autonomous {
     public void init() {
         Robot.setup(hardwareMap, telemetry);
         double wheelDiameter = 96/MM_PER_INCH;
-        _Motor fr = new _Motor("motorFR", _Motor.Type.GOBILDA_435_RPM, DcMotorSimple.Direction.FORWARD,
+        _Motor fr = new _Motor("motorFR", _Motor.Type.GOBILDA_312_RPM, DcMotorSimple.Direction.FORWARD,
                 DcMotor.ZeroPowerBehavior.BRAKE, wheelDiameter, true);
-        _Motor fl = new _Motor("motorFL", _Motor.Type.GOBILDA_435_RPM, DcMotorSimple.Direction.FORWARD,
+        _Motor fl = new _Motor("motorFL", _Motor.Type.GOBILDA_312_RPM, DcMotorSimple.Direction.FORWARD,
                 DcMotor.ZeroPowerBehavior.BRAKE, wheelDiameter, true);
-        _Motor br = new _Motor("motorBR", _Motor.Type.GOBILDA_435_RPM, DcMotorSimple.Direction.FORWARD,
+        _Motor br = new _Motor("motorBR", _Motor.Type.GOBILDA_312_RPM, DcMotorSimple.Direction.FORWARD,
                 DcMotor.ZeroPowerBehavior.BRAKE, wheelDiameter, true);
-        _Motor bl = new _Motor("motorBL", _Motor.Type.GOBILDA_435_RPM, DcMotorSimple.Direction.FORWARD,
+        _Motor bl = new _Motor("motorBL", _Motor.Type.GOBILDA_312_RPM, DcMotorSimple.Direction.FORWARD,
                 DcMotor.ZeroPowerBehavior.BRAKE, wheelDiameter, true);
         _drivetrain = new _Drivetrain(fr, fl, br, bl, 1.0);
         _drivetrain.setTypicalSpeed(0.5);
