7a54266894c287f31a3a3e8322e47ac7b5808015
Rohan Dugad <rohand2468@gmail.com>
2023-01-29 22:08:19 -0500

    Updated TFODTest with custom model

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverTests/TFODTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverTests/TFODTest.java
@@ -18,18 +18,14 @@ public class TFODTest extends _Autonomous {
     public void init() {
         Robot.setup(hardwareMap, telemetry);
         _Vuforia vuforia = new _Vuforia("Webcam 1");
-        _tfod = new _TFOD(vuforia.getVuforia(), 0.45f, true, 320, 1.1, 16.0/9.0,
-                "FreightFrenzy_BCDM.tflite", new String[] {"Ball", "Cube", "Duck", "Marker"});
-        _validRecognition = recognition -> ((recognition.getWidth() * recognition.getHeight()) < (320.0 * 640.0 / 8.0)) && (recognition.getWidth()/recognition.getHeight()) > 1.0;
+        _tfod = new _TFOD(vuforia.getVuforia(), 0.45f, true, 320, 1.0, 16.0/9.0,
+                "AGS1273.tflite", new String[] {"robot", "gear", "android"});
+        _validRecognition = recognition -> true;
     }
 
     @Override
     public void init_loop() {
         telemetry.addLine(_tfod.getLatestRecognitions() != null ? _tfod.getRecognitions().toString() : "NULL");
-
-        Recognition temp = _tfod.getRecognitionValidLabel(_validRecognition, "Marker");
-        telemetry.addLine(String.valueOf(_tfod.countValidLabel(_validRecognition, "Marker")));
-        telemetry.addLine(temp != null ? temp.toString() : "NULL");
     }
 
     @Override
